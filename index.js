08c00229463f5a77b258ccf4c87acef1dce10d85
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-09-12 02:40:46 +0300

    Add

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const PORT = process.env.PORT || 3000;
 
 express()
     //.use(express.static(path.join(__dirname, 'public')))
-    .get('/', (req, res) => res.send("Hello"))
+    .get('/', (req, res) => res.send("Hell"))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
